3757b26ac50793af5d4cac00891daa66a1cb0dd8
Kadek Dodik Wirawan <dodikdogen@gmail.com>
2022-02-06 10:05:32 +0800

    init commit

---
+++ src/components/AllSection.jsx
@@ -0,0 +1,101 @@
+export default function AllSection(){
+    return(
+        <>
+<div>
+  <section>
+    <div className="row pt-5">
+      <div className="col-4">
+        <img src="./img/sampo-hitam.webp" className="img-fluid" alt="sampo-hitam" />
+      </div>
+      <div className="col align-self-center">
+        <h3 className="mb-1 bg-danger text-white p-1 me-4">Cultusia Sampoo</h3>
+        <h1 className="lh-1 display-3 fw-normal text-uppercase"><span className="fw-bold">Hitamkan Uban</span> dengan <span className="fw-bold">Sekali Keramas</span>
+        </h1>
+        <hr className="mb-0 me-5" />
+        <p>yang membuat rambut hitam berkilau.</p>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div className="pe-3 ps-3">
+      <p>Shampoo khusus untuk menutup uban dengan extract jahe dan minyak zaitun dan bebas dari kandungan AMONIA
+        Praktis, Efisien dan Cepat. Cukup pakai seperti sampo biasa.
+        Cukup 5 menit uban tertutup semua. Rambut jadi hitam alami, wangi dan lembut.</p>
+    </div>
+    <img src="./img/sampocultusia.webp" className="img-fluid" alt="sampocultusia" />
+    <h3 className="p-2 bg-danger text-white">Sampo Cultusia Lebih unggul Karena :</h3>
+    <ul>
+      <li><strong className="text-danger">100% bahan alami dan ramah lingkungan</strong>, tidak mengandung amonia, kaya akan ekstrak tumbuhan, menutrisi rambut dan melindungi kulit kepala</li>
+      <li><strong className="text-danger">Efektif Untuk Menutupi Rambut Uban</strong> Kini anda tidak perlu kesalon lagi untuk mewarnai rambut uban.</li>
+      <li><strong className="text-danger">Aman dan Nyaman</strong> Sudah terdaftar BPOM dengan Nomor Registrasi NA18171003144</li>
+      <li><strong className="text-danger">Lebih Ekonomis dan Praktis</strong> cukup keramas seperti biasa rambut uban tertutupi</li>
+    </ul>
+    <div className="text-center p-3">
+      <h1 className="fw-bold">Testimonial</h1>
+      <p className="lh-sm fst-italic">Simak testimoni mereka yang sudah mendapatkan manfaatnya</p>
+      <div className="row">
+        <div className="col">
+          <img src="./img/testimoni1.webp" className="img-fluid" alt="customer1" />
+        </div>
+        <div className="col">
+          <img src="./img/testimoni2.webp" className="img-fluid" alt="customer2" />
+        </div>
+      </div>
+      <div className="row pt-3">
+        <div className="col">
+          <img src="./img/testimoni3.webp" className="img-fluid" alt="customer3" />
+        </div>
+        <div className="col">
+          <img src="./img/testimoni4.webp" className="img-fluid" alt="customer4" />
+        </div>
+      </div>
+    </div>
+  </section>
+  <div>
+    <h3 className="bg-danger p-3 text-white">Cara Penggunaan Sampo Cultusia:</h3>
+    <div className="pe-3 ps-3">
+      <ol>
+        <li>Keluarkan isi sampoo cultusia secukupnya di telapak tangan</li>
+        <li>Usapkan di semua bagian rambut sampai berbusa</li>
+        <li>Tunggu 5 sampai dengan 10 menit</li>
+        <li>Bilas dengan air bersih</li>
+        <li>Bisa digunakan saat rambut kering maupun basah.</li>
+      </ol>
+    </div>
+  </div>
+  <section>
+    <div className="p-3 text-center">
+      <h5>Saya Mau! Berapa harga Sampo Cultusia ?</h5>
+      <h1 className="text-uppercase fw-bold text-danger">Promo Terbatas</h1>
+      <h1 className="text-uppercase text-primary">Diskon + Gratis Ongkir Seluruh Indoneisa</h1>
+      <h3>Harga Normal : <span className="text-decoration-line-through text-danger">Rp 289.000</span></h3>
+      <h3>Harga Hemat :</h3>
+      <h1 className="fw-bold display-1 text-success">Rp 189.000</h1>
+      <h5>Sudah <span className="fw-bold text-danger text-uppercase">Gratis Ongkir</span> Ke Alamat Anda</h5>
+      <p>Untuk Pemesanan atau mau tanya-tanya klik tombol di bawah ini dan anda akan terhubung dengan cs kami lewat whatsapp</p>
+      <img src="./img/down.webp" className="img-fluid" alt="down" />
+      <div className="d-grid gap-2">
+        <a id="atc" href="whatsapp://send?phone=6281414170497&text=Halo%20tanya%20dong%20Shampo%20Cultusia%20untuk%20hitamkan%20rambut%20uban%20sekali%20keramas%20%20lagi%20ada%20promo%20dan%20Bonus%20GRATIS%20ongkir%20ya%20%3F%3F" className="btn btn-success btn-xl text-uppercase">Mau Diskon + Gratis Ongkir, Klik Disini</a>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div className="p-3">
+      <h3>Kenapa Memilih Belanja Disini :</h3>
+      <ul>
+        <li>Jika paket yang dikirim tidak sesuai dengan pesanan anda, Bisa di kembalikan dengan pengembalian dana 100% tanpa potongan.</li>
+        <li>Menerima pembayaran COD atau Bayar setelah paket sampai dirumah.</li>
+        <li>Packing aman dan Privacy anda sebagai pembeli aman di dalam database kami.</li>
+      </ul>
+      <p className="fs-5 lh-sm">Nb : Kini saatnya anda bisa tutupi uban tanpa perlu pergi ke salon menunggu berjam-jam, Cukup keramas dengan Cultusia sampo rambut uban tertutupi.</p>
+      <img src="./img/down.webp" className="img-fluid" alt="down" />
+      <div className="d-grid gap-2">
+        <a id="atc" href="whatsapp://send?phone=6281414170497&text=Halo%20tanya%20dong%20Shampo%20Cultusia%20untuk%20hitamkan%20rambut%20uban%20sekali%20keramas%20%20lagi%20ada%20promo%20dan%20Bonus%20GRATIS%20ongkir%20ya%20%3F%3F" className="btn btn-success btn-xl text-uppercase">PESAN LEWAT COD, KLIK DISINI</a>
+      </div>
+    </div>
+  </section>
+</div>
+
+        </>
+    )
+}
